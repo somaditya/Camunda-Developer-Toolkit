ba2a7ba7575c102207bb06ab9f9f01c41017ece3
Somaditya Basak <mail@somaditya.com>
2020-10-28 01:59:11 +0530

    added java-ee demos

---
+++ Camunda-Java-EE/src/main/java/org/camunda/demo/SimpleDemo/CheckWeatherDelegate.java
@@ -0,0 +1,20 @@
+package org.camunda.demo.SimpleDemo;
+
+import java.util.Random;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class CheckWeatherDelegate implements JavaDelegate {
+	
+	static int count = 0;
+
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		
+		execution.setVariable("name", "Somaditya Basak");
+		execution.setVariable("count", ++count);
+		
+	}
+
+}
